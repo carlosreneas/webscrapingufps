bc49df65b2966b41b608221eade3abea925f3fae
carlosreneas <carlosreneas@ufps.edu.co>
2022-11-10 15:52:57 -0500

    Update covid.py

--- covid.py
+++ covid.py
@@ -47,3 +47,15 @@ while True:
  
 # Sort the data by the number of confirmed cases
 data.sort(key = lambda row: row[1], reverse = True)
+
+table = tt.Texttable()
+ 
+# Add an empty row at the beginning for the headers
+table.add_rows([(None, None, None, None)] + data)
+ 
+# 'l' denotes left, 'c' denotes center,
+# and 'r' denotes right
+table.set_cols_align(('c', 'c', 'c', 'c')) 
+table.header((' Country ', ' Number of cases ', ' Deaths ', ' Continent '))
+ 
+print(table.draw())
